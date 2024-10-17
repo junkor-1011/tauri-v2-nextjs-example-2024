87756625ad0df30fa79a2f77af889ddbe027cf58
junkor-1011 <tomoyukizxy@gmail.com>
2024-10-18 00:19:20 +0900

    format className order for tailwind-css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
 
+/** tmp */
+const blank = ' ';
+
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
   variable: '--font-geist-sans',
@@ -26,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable}${blank}${geistMono.variable} antialiased`}
       >
         {children}
       </body>
